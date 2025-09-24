8b7201237a911ee2538ae0d4a21d3c01aba698dc
rdsarjito <rama.dhaninursarjito@gmail.com>
2025-09-24 19:05:59 +0700

    [UPDATE]ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import js from "@eslint/js";
-import nextPlugin from "eslint-plugin-next";
+import next from "eslint-config-next";
 import tseslint from "typescript-eslint";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  nextPlugin.configs["core-web-vitals"],
+  ...next(),
   prettierRecommended,
   {
     rules: {
